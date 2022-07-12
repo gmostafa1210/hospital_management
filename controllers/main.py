202dff22b108923e68368fe9da52f1a30774acfe
gm <golam.mostafa@bistasolutions.com>
2022-07-12 23:39:15 +0600

    update

--- controllers/main.py
+++ controllers/main.py
@@ -6,12 +6,12 @@ from odoo.http import request
 
 class HospitalAppointment(http.Controller):
 
-    @http.route('/create/appointment_form', type="http", auth="public", website=True)
-    def take_appointment_form(self, **kw):
-        patient_history_rec = request.env['hospital.patient.history'].sudo().search([])
-        return http.request.render('om_hospital.take_appointment_form', {})
+    @http.route('/appointment_form', type="http", auth="public", website=True)
+    def take_appointment(self, **kw):
+        # patient_history_rec = request.env['hospital.patient.history'].sudo().search([])
+        return http.request.render('hospital_management.take_appointment_form', {})
 
-    @http.route('/create/appointment_form', type="http", auth="public", website=True)
+    @http.route('/create/appointment', type="http", auth="public", website=True)
     def create_appointment_form(self, **kw):
-        request.env['hospital.patient.history'].sudo().create(kw)
-        return request.render("om_hospital.patient_thanks", {})
+        # request.env['hospital.patient.history'].sudo().create(kw)
+        return request.render("hospital_management.patient_thanks", {})
