6e9d98d5c5c8f3fc651fcddfbaf217f7d27968b7
gm <golam.mostafa@bistasolutions.com>
2022-07-14 12:50:51 +0600

    update

--- controllers/main.py
+++ controllers/main.py
@@ -8,17 +8,21 @@ class HospitalAppointment(http.Controller):
 
     @http.route('/appointment_form', type="http", auth="public", website=True)
     def take_appointment(self, **kw):
-        # patient_history_rec = request.env['hospital.patient.history'].sudo().search([])
         return http.request.render('hospital_management.take_appointment_form', {})
 
-    @http.route('/create/appointment', type="http", auth="public", website=True)
+    @http.route('/create/patient', type="http", auth="public", website=True)
     def create_patient(self, **kw):
         if kw:
             phone_number = kw['phone']
             patient_rec = request.env['hospital.patient'].sudo().search([('phone', '=', phone_number)])
-        # request.env['hospital.patient'].sudo().create(kw)
-        if patient_rec:
-            print("##################Patient Already Exist", patient_rec)
-        else:
-            request.env['hospital.patient'].sudo().create(kw)
+            hospital_rec = request.env['hospital.hospital'].sudo().search([])
+            doctor_rec = request.env['hospital.doctor'].sudo().search([])
+            if not patient_rec:
+                request.env['hospital.patient'].sudo().create(kw)
+            new_patient_rec = request.env['hospital.patient'].sudo().search([('phone', '=', phone_number)])
+            return http.request.render('hospital_management.patient_appointment', {'new_patient_rec': new_patient_rec, 'hospital_rec': hospital_rec, 'doctor_rec': doctor_rec})
+
+    @http.route('/create/appointment', type="http", auth="public", website=True)
+    def create_appointment(self, **kw):
+        request.env['hospital.patient.history'].sudo().create(kw)
         return request.render("hospital_management.patient_thanks", {})
